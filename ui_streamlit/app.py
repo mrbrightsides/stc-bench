b59e5622bdbc1a7ea2cbf99b0dcaba67dd2ca054
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-15 20:11:30 +0700

    Update iframe URL to hide more top space

--- ui_streamlit/app.py
+++ ui_streamlit/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # iframe URL dengan hideTop=100
-iframe_url = "https://ohara.ai/mini-apps/a11f2bf3-af2b-4763-aeb8-53999129c2e5?hideTop=100"
+iframe_url = "https://ohara.ai/mini-apps/a11f2bf3-af2b-4763-aeb8-53999129c2e5?hideTop=1000"
 
 # tampilkan iframe full-screen
 st.markdown(
